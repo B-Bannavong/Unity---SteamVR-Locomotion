b4e1b3deacc387546128f78e2f551d662800df2a
Brandon Bannavong <57028677+B-Bannavong@users.noreply.github.com>
2021-12-29 15:44:40 -0500

    Initial Commit

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform playerCenter;
+    public CharacterController character;
+    public SteamVR_Action_Vector2 touchPadInput; 
+    public float speed = 5;
+    private CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(touchPadInput.axis.x, 0, touchPadInput.axis.y));
+        character.center = new Vector3(playerCenter.localPosition.x, 1.03f, playerCenter.localPosition.z);
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0)*Time.deltaTime);
+    }
+}
